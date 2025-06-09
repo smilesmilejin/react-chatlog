7d810b4bf09cc1943c7158991f08fe767fba8e8c
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-09 17:04:52 -0400

    completed Wave 1 ChatEntry

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry.jsx';
 
 const App = () => {
   return (
@@ -9,6 +10,14 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry></ChatEntry>
+        {/* <ChatEntry
+          id={7}
+          sender="Joe Biden"
+          body="Get out by 8am.  I'll count the silverware"
+          timeStamp="2018-05-18T22:12:03Z"
+          liked={false}
+        /> */}
       </main>
     </div>
   );
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,12 +1,43 @@
 import './ChatEntry.css';
+import TimeStamp from './TimeStamp.jsx';
 
-const ChatEntry = () => {
+const ChatEntry = (props) => {
+  // Wave 1 try
+  // const m = {
+  //   'id': 1,
+  //   'sender':'Vladimir',
+  //   'body':'why are you arguing with me',
+  //   'timeStamp':'2018-05-29T22:49:06+00:00',
+  //   'liked': false
+  // };
+
+  //   return (
+  //   <div className="chat-entry local">
+  //     {/* <h2 className="entry-name">Replace with name of sender</h2> */}
+  //     <h2 className="entry-name">{m.sender}</h2>
+  //     <section className="entry-bubble">
+  //       {/* <p>Replace with body of ChatEntry</p>
+  //       <p className="entry-time">Replace with TimeStamp component</p> */}
+
+  //       <p>{m.body}</p>
+  //       <p className="entry-time"><TimeStamp time={m.timeStamp}></TimeStamp></p>
+
+  //       <button className="like">🤍</button>
+  //     </section>
+  //   </div>
+  // );
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      {/* <h2 className="entry-name">Replace with name of sender</h2> */}
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        {/* <p>Replace with body of ChatEntry</p>
+        <p className="entry-time">Replace with TimeStamp component</p> */}
+
+        <p>{props.body}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp}></TimeStamp></p>
+
         <button className="like">🤍</button>
       </section>
     </div>
